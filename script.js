115eaa5972ede39d3b4c6ed7c7d076357583a884
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-22 04:25:08 +0500

    code is added

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const apikey = "88c22151468d8609cbdce07b808891ff"
-const search = document.querySelector('.search')
-const input = document.querySelector('#city-name')
-const searchbtn = document.querySelector('.btn');
-const weatherdata = document.querySelector('.weather')
-const icon = document.createElement('weather-icon');
+const apikey = "88c22151468d8609cbdce07b808891ff";
+const search = document.querySelector(".search");
+const input = document.querySelector("#city-name");
+const searchbtn = document.querySelector(".btn");
+const weatherdata = document.querySelector(".weather");
+const icon = document.createElement("weather-icon");
 
-searchbtn.addEventListener('submit',function(e) {
-    e.preventDefault();
-    // console.log(input.value);   
-    const cityname = input.value;
-    getWeatherData(cityname)
+search.addEventListener("click", function (e) {
+  e.preventDefault();
+//   console.log(input.value);
+  const cityname = input.value;
+  getWeatherData(cityname);
 });
 
-async function getWeatherData() {
-    try{
-        const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apikey}&units=metric`)
-        if(!response.ok) {
-            throw new Error("Network response is not ok!");
-        }
-        const data = response.json();
-    }catch(err){
-
+async function getWeatherData(cityname) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apikey}&units=metric`
+    );
+    if (!response.ok) {
+      throw new Error("Network response is not ok!");
     }
-
+    const data = await response.json();
+    console.log(data);
+    Math.floor(data.main.temp)
+  } catch (err) {}
 }
