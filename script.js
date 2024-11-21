f88d3d4f57128c7a5bbb334910e69dd45d6baddd
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-22 03:54:33 +0500

    code is added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const apikey = "88c22151468d8609cbdce07b808891ff"
+const search = document.querySelector('.search')
+const input = document.querySelector('#city-name')
+const searchbtn = document.querySelector('.btn');
 const weatherdata = document.querySelector('.weather')
-const input = document.createElement('#city-name')
-const searchbtn = document.createElement('search');
 const icon = document.createElement('weather-icon');
 
 searchbtn.addEventListener('click',function(e) {
-    
+    // console.log(input.value);   
+    const cityname = input.value;
 });
